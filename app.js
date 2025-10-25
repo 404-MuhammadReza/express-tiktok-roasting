1534ba4c52f49abdb9bd969a1e62c54d77ee07ee
404-MuhammadReza <vt7trjm7y2@privaterelay.appleid.com>
2025-10-26 00:31:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = ['http://localhost:5173', 'https://frontend-tiktok-roasting-7zmoao8et.vercel.app'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Memungkinkan permintaan tanpa origin (misalnya, dari aplikasi seluler atau permintaan file)
-    if (!origin) return callback(null, true); 
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 const routes = require('./routes/route');
 app.use('/api/', routes);
