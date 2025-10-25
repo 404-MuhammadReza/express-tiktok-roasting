4ec28ce708a069c317d91ce6d44ee0a3a54d0176
404-MuhammadReza <vt7trjm7y2@privaterelay.appleid.com>
2025-10-25 22:44:39 +0700

    add CORS

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:5173', 'https://domain-frontend-anda.vercel.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Memungkinkan permintaan tanpa origin (misalnya, dari aplikasi seluler atau permintaan file)
+    if (!origin) return callback(null, true); 
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const routes = require('./routes/route');
 app.use('/api/', routes);
